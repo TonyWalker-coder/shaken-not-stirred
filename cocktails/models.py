7b7ac2f81ebee5fa04979fcc036a58f63fa20599
TonyWalker-coder <TONYWALKERSMAIL@GMAIL.COM>
2026-04-17 16:22:04 +0100

    Remove .env from repository and add to .gitignore

--- cocktails/models.py
+++ cocktails/models.py
@@ -3,7 +3,11 @@ from django.db import models
 # Create your models here.
 class Cocktail(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    ingredients = models.TextField(blank=True)
+    method = models.TextField(blank=True)
+    image_url = models.URLField(blank=True)
+    glass_type = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return self.name
-    
