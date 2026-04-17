6b36f41fadce559f7232fe547c1ec65c0d66f9a8
TonyWalker-coder <TONYWALKERSMAIL@GMAIL.COM>
2026-04-17 16:51:14 +0100

    added cocktail page

--- shakenstirred/urls.py
+++ shakenstirred/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cocktails.views import cocktail_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cocktails/', cocktail_list, name='cocktail_list'),
 ]
