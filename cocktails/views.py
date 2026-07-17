fa2875316b95d99a42a97a00a94d3aceb5458438
TonyWalker-coder <TONYWALKERSMAIL@GMAIL.COM>
2026-07-17 11:40:59 +0100

    changed grid layout and sorted cocktail list

--- cocktails/views.py
+++ cocktails/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Cocktail
 
 # Create your views here.
+from django.db.models.functions import Lower
+
 def cocktail_list(request):
-    cocktails = Cocktail.objects.all()
+    cocktails = Cocktail.objects.all().order_by(Lower("name"))
     return render(request, 'cocktails/cocktail_list.html', {'cocktails': cocktails})
