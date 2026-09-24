9ca2aabeab559f83d72bf3ff73db65a675725b9d
TonyWalker-coder <TONYWALKERSMAIL@GMAIL.COM>
2026-09-24 11:25:27 +0100

    getting debug false to work

--- shakenstirred/settings.py
+++ shakenstirred/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -144,6 +145,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
